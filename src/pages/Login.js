3f6c265bdff2ad1c22fbbfa59fcf852533b66477
Tim <Timmvanc@gmail.com>
2025-02-10 18:02:39 -0500

    Fixed admin login API request to match backend route

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,16 +10,15 @@ const Login = () => {
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
-      e.preventDefault();
-      try {
-        const response = await axiosInstance.post('/auth/admin-login', { email, password }); 
-        localStorage.setItem('adminToken', response.data.token);
-        navigate('/overview'); // Redirect to the overview page after successful login
-      } catch (error) {
-        setErrorMessage('Invalid login credentials');
-      }
-  };
-  
+    e.preventDefault();
+    try {
+      const response = await axiosInstance.post('/auth/admin/login', { email, password });
+      localStorage.setItem('adminToken', response.data.token);
+      navigate('/overview'); // Redirect to the overview page after successful login
+    } catch (error) {
+      setErrorMessage('Invalid login credentials');
+    }
+};
 
   return (
     <div className="login-container">
